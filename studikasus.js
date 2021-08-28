ca51074421c0a1f9fc3e3bc0ed8811e0c76beda8
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-28 18:25:41 +0700

    Tugas Studi Kasus Javascript
    tentang membalikan kata dengan string revese

---
+++ studikasus.js
@@ -0,0 +1,7 @@
+function balikKata(kata){
+    return kata.split('').reverse().join('');
+}
+console.log(balikKata("Niomic!"))    
+console.log(balikKata("JavaScript"))  
+console.log(balikKata("alohahola"))  
+console.log(balikKata("Jawa_Barat")) 
